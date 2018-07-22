415e886d4d9d10588379541acddf3967748207ea
Lynix <lynix680@gmail.com>
2018-07-22 18:55:42 +0200

    Add mention module

---
+++ module_mention.lua
@@ -0,0 +1,34 @@
+-- Copyright (C) 2018 Jérôme Leclercq
+-- This file is part of the "Not a Bot" application
+-- For conditions of distribution and use, see copyright notice in LICENSE
+
+local client = Client
+local config = Config
+local discordia = Discordia
+local bot = Bot
+
+Module.Name = "mention"
+
+function Module:OnMessageCreate(message)
+	local mention = false
+
+	if (message.mentionsEveryone) then
+		mention = true
+	else
+		for _, user in pairs(message.mentionedUsers) do
+			if (user.id == client.user.id) then
+				mention = true
+				break
+			end
+		end
+	end
+
+	if (mention) then
+		local mentionEmoji = bot:GetEmojiData("mention")
+		if (not mentionEmoji or not mentionEmoji.Emoji) then
+			return
+		end
+
+		message:addReaction(mentionEmoji.Emoji)
+	end
+end
