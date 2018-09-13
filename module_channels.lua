902b548cee10514abb714b7f83ec80b466bcaccd
Lynix <lynix680@gmail.com>
2018-09-13 20:30:23 +0200

    Channels: Add updatechannelconfig command

--- module_channels.lua
+++ module_channels.lua
@@ -7,6 +7,8 @@ local client = Client
 local discordia = Discordia
 local enums = discordia.enums
 
+discordia.extensions() -- load all helpful extensions
+
 Module.Name = "channels"
 
 function Module:GetConfigTable()
@@ -21,24 +23,26 @@ function Module:GetConfigTable()
 end
 
 function Module:OnLoaded()
-	Bot:RegisterCommand({
+	self:RegisterCommand({
 		Name = "channelconfig",
-		Args = {},
+		Args = {
+			{Name = "configMessage", Type = Bot.ConfigType.Message, Optional = true}
+		},
 		PrivilegeCheck = function (member) return member:hasPermission(enums.permission.administrator) end,
 
-		Help = "Configures the channel module messages and reactions",
-		Func = function (message)
+		Help = "Lists the channel module messages and reactions",
+		Func = function (commandMessage, configMessage)
 			local guild = commandMessage.guild
 			local config = self:GetConfig(guild)
 			local data = self:GetData(guild)
-			local channelConfig = config.ChannelConfig
 
+			local channelConfig = config.ReactionActions
 			for channelId,messageTable in pairs(channelConfig) do
 				local channel = guild:getChannel(channelId)
 				if (channel) then
 					for messageId,reactionTable in pairs(messageTable) do
 						local message = channel:getMessage(messageId)
-						if (message) then
+						if (message and (not configMessage or configMessage.id == message.id)) then
 							local fields = {}
 							for k,reactionInfo in pairs(reactionTable) do
 								local roleActions = self:GetReactionActions(guild, channelId, messageId, reactionInfo.reaction, true)
@@ -67,11 +71,11 @@ function Module:OnLoaded()
 								local actions = ""
 
 								if (#addedRoles > 0) then
-									actions = string.format("%s**Adds role%s**\n-  %s\n", actions, #addedRoles > 1 and "s" or "", table.concat(addedRoles, "\n-  "))
+									actions = string.format("%s**Adds role%s** %s\n", actions, #addedRoles > 1 and "s" or "", table.concat(addedRoles, ", "))
 								end
 
 								if (#removedRoles > 0) then
-									actions = string.format("%s**Removes role%s:**\n-  %s\n", actions, #removedRoles > 1 and "s" or "", table.concat(removedRoles, "\n-  "))
+									actions = string.format("%s**Removes role%s** %s\n", actions, #removedRoles > 1 and "s" or "", table.concat(removedRoles, ", "))
 								end
 
 								if (roleActions.Message) then
@@ -81,7 +85,7 @@ function Module:OnLoaded()
 								local emoji = bot:GetEmojiData(guild, reactionInfo.reaction)
 
 								table.insert(fields, {
-									name = emoji and emoji.MentionString or "<invalid emoji>",
+									name = string.format("- %s:", emoji and emoji.MentionString or "<invalid emoji>"),
 									value = actions
 								})
 							end
@@ -89,7 +93,10 @@ function Module:OnLoaded()
 							commandMessage:reply({
 								embed = { 
 									description = string.format("Message in %s:\n%s", channel.mentionString, bot:GenerateMessageLink(message)),
-									fields = fields
+									fields = fields,
+									footer = {
+										text = "Use `!updatechannelconfig <message link> <emoji> <action>` to update channels reactions settings."
+									}
 								}
 							})
 						end
@@ -99,11 +106,159 @@ function Module:OnLoaded()
 		end
 	})
 
-	return true
-end
+	self:RegisterCommand({
+		Name = "updatechannelconfig",
+		Args = {
+			{Name = "message", Type = Bot.ConfigType.Message},
+			{Name = "emoji", Type = Bot.ConfigType.Emoji},
+			{Name = "action", Type = Bot.ConfigType.String},
+			{Name = "value", Type = Bot.ConfigType.String, Optional = true}
+		},
+		PrivilegeCheck = function (member) return member:hasPermission(enums.permission.administrator) end,
 
-function Module:OnUnload()
-	bot:UnregisterCommand("channelconfig")
+		Help = "Configures the channel module messages and reactions",
+		Func = function (commandMessage, message, emoji, action, value)
+			local guild = commandMessage.guild
+			local config = self:GetConfig(guild)
+
+			local GetReactionActionsConfig = function (channelId, messageId, reaction, noCreate)
+				local guildConfig = config.ReactionActions
+
+				local channelTable = guildConfig[channelId]
+				if (not channelTable) then
+					if (noCreate) then
+						return
+					end
+
+					channelTable = {}
+					guildConfig[channelId] = channelTable
+				end
+
+				local messageTable = channelTable[messageId]
+				if (not messageTable) then
+					if (noCreate) then
+						return
+					end
+
+					messageTable = {}
+					channelTable[messageId] = messageTable
+				end
+
+				local reactionActions = messageTable[reaction]
+				if (not reactionActions) then
+					if (noCreate) then
+						return
+					end
+
+					reactionActions = {}
+					messageTable[reaction] = reactionActions
+				end
+
+				return reactionActions
+			end
+
+			local success = false
+
+			if (action == "addrole") then
+				local role = guild:getRole(value)
+				if (not role) then
+					commandMessage:reply(string.format("Invalid role %s", value))
+					return
+				end
+
+				local roleValue = tostring(role.id)
+
+				local reactionActions = GetReactionActionsConfig(message.channel.id, message.id, emoji.Name)
+				if (reactionActions.AddRoles) then
+					local found = false
+					for _, role in pairs(reactionActions.AddRoles) do
+						if (role == roleValue) then
+							found = true
+							break
+						end
+					end
+
+					for key, role in pairs(reactionActions.RemoveRoles) do
+						if (role == roleValue) then
+							table.remove(reactionActions.RemoveRoles, key)
+						end
+					end
+
+					if (not found) then
+						table.insert(reactionActions.AddRoles, tostring(role.id))
+					end
+				else
+					reactionActions.AddRoles = {roleValue}
+				end
+
+				success = true
+				commandMessage:reply(string.format("Reactions on %s for %s will now adds role %s (%s)", Bot:GenerateMessageLink(message), emoji.MentionString, role.name, role.id))
+			elseif (action == "removerole") then
+				local role = guild:getRole(value)
+				if (not role) then
+					commandMessage:reply(string.format("Invalid role %s", value))
+					return
+				end
+
+				local roleValue = tostring(role.id)
+
+				local reactionActions = GetReactionActionsConfig(message.channel.id, message.id, emoji.Name)
+				if (reactionActions.RemoveRoles) then
+					local found = false
+					for _, role in pairs(reactionActions.RemoveRoles) do
+						if (role == roleValue) then
+							found = true
+							break
+						end
+					end
+
+					for key, role in pairs(reactionActions.AddRoles) do
+						if (role == roleValue) then
+							table.remove(reactionActions.AddRoles, key)
+						end
+					end
+
+					if (not found) then
+						table.insert(reactionActions.RemoveRoles, tostring(role.id))
+					end
+				else
+					reactionActions.RemoveRoles = {roleValue}
+				end
+
+				success = true
+				commandMessage:reply(string.format("Reactions on %s for %s will now remove role %s (%s)", Bot:GenerateMessageLink(message), emoji.MentionString, role.name, role.id))
+			elseif (action == "send") then
+				if (not value) then
+					commandMessage:reply("Empty message")
+					return
+				end
+
+				local reactionActions = GetReactionActionsConfig(message.channel.id, message.id, emoji.Name)
+				reactionActions.SendMessage = value
+
+				success = true
+				commandMessage:reply(string.format("Reactions on %s for %s will now send private message: %s", Bot:GenerateMessageLink(message), emoji.MentionString, value))
+			elseif (action == "clear") then
+				local reactionActions = GetReactionActionsConfig(message.channel.id, message.id, emoji.Name, true)
+				if (reactionActions) then
+					reactionActions.AddRoles = nil
+					reactionActions.RemoveRoles = nil
+					reactionActions.SendMessage = nil
+				end
+
+				success = true
+				commandMessage:reply(string.format("Reactions actions on %s for %s have been cleared", Bot:GenerateMessageLink(message), emoji.MentionString))
+			else
+				commandMessage:reply("Invalid action (must be addrole/clear/removerole/send)")
+			end
+
+			if (success) then
+				self:SaveConfig()
+				commandMessage:reply(string.format("Configuration of module %s has been saved, use the `!reload %s` command to activate it", self.Name, self.Name))
+			end
+		end
+	})
+	return true
 end
 
 function Module:GetReactionActions(guild, channelId, messageId, reaction, noCreate)
@@ -128,15 +283,51 @@ function Module:OnEnable(guild)
 	local data = self:GetData(guild)
 	data.ReactionActions = {}
 
-	local t1 = os.clock()
+	--[[if (config.ChannelConfig) then
+		self:LogInfo(guild, "Converting old config format to new config format...")
 
-	local ProcessRole = function (channelId, messageId, reaction, roleId)
-		local remove = false
-		if (roleId:sub(1,1) == "-") then
-			roleId = roleId:sub(2)
-			remove = true
+		local guildConfig = {}
+		for channelId,messagetable in pairs(config.ChannelConfig) do
+			local channelMessages = {}
+			guildConfig[channelId] = channelMessages
+
+			for messageId,reactionTable in pairs(messagetable) do
+				local messageActions = {}
+				channelMessages[messageId] = messageActions
+
+				for k,reactionInfo in pairs(reactionTable) do
+					local reactionActions = {}
+					messageActions[reactionInfo.reaction] = reactionActions
+
+					if (reactionInfo.roles) then
+						for k,roleId in pairs(reactionInfo.roles) do
+							if (roleId:sub(1,1) ~= "-") then
+								reactionActions.AddRoles = reactionActions.AddRoles or {}
+								table.insert(reactionActions.AddRoles, roleId)
+							else
+								roleId = roleId:sub(2)
+
+								reactionActions.RemoveRoles = reactionActions.RemoveRoles or {}
+								table.insert(reactionActions.RemoveRoles, roleId)
+							end
+						end
+					end
+
+					if (reactionInfo.message) then
+						reactionActions.SendMessage = reactionInfo.message
+					end
+				end
+			end
 		end
+		config.ReactionActions = guildConfig
 
+		config.ChannelConfig = nil
+		self:SaveConfig(guild)
+	end]]
+
+	self:LogInfo(guild, "Processing roles...")
+
+	local ProcessRole = function (channelId, messageId, reaction, roleId, remove)
 		if (guild:getRole(roleId)) then
 			local roleActions = self:GetReactionActions(guild, channelId, messageId, reaction)
 			if (not remove) then
@@ -149,20 +340,26 @@ function Module:OnEnable(guild)
 		end
 	end
 
-	self:LogInfo(guild, "Processing roles...")
+	for channelId,messageTable in pairs(config.ReactionActions) do
+		for messageId,reactionTable in pairs(messageTable) do
+			for reaction, actions in pairs(reactionTable) do
+				local roleActions = self:GetReactionActions(guild, channelId, messageId, reaction)
 
-	for channelId,messagetable in pairs(config.ChannelConfig) do
-		for messageId,reactionTable in pairs(messagetable) do
-			for k,reactionInfo in pairs(reactionTable) do
-				if (reactionInfo.roles) then
-					for k,roleId in pairs(reactionInfo.roles) do
-						ProcessRole(channelId, messageId, reactionInfo.reaction, roleId)
+				if (actions.AddRoles) then
+					for _,roleId in pairs(actions.AddRoles) do
+						ProcessRole(channelId, messageId, reaction, roleId, false)
 					end
 				end
 
-				if (reactionInfo.message) then
-					local roleActions = self:GetReactionActions(guild, channelId, messageId, reactionInfo.reaction)
-					roleActions.Message = reactionInfo.message
+				if (actions.RemoveRoles) then
+					for _,roleId in pairs(actions.RemoveRoles) do
+						ProcessRole(channelId, messageId, reaction, roleId, true)
+					end
+				end
+
+				if (actions.SendMessage) then
+					local roleActions = self:GetReactionActions(guild, channelId, messageId, reaction)
+					roleActions.Message = actions.SendMessage
 				end
 			end
 		end
@@ -171,33 +368,40 @@ function Module:OnEnable(guild)
 	self:LogInfo(guild, "Adding emojis to concerned messages...")
 
 	-- Make sure reactions are present on messages
-	for channelId,messagetable in pairs(config.ChannelConfig) do
+	for channelId,messageTable in pairs(config.ReactionActions) do
 		local channel = guild:getChannel(channelId)
-		for messageId,reactionTable in pairs(messagetable) do
-			local message = channel:getMessage(messageId)
-			
-			local hasReaction = {}
-			for k,reaction in pairs(message.reactions) do
-				if (reaction.me) then
-					local emoji = bot:GetEmojiData(guild, reaction.emojiName)
-					if (emoji) then
-						hasReaction[emoji.Name] = true
-					else
-						self:LogError(guild, "Found reaction which does not exist in guild: %s", reaction.emojiName)
+		if (channel) then
+			for messageId,reactionTable in pairs(messageTable) do
+				local message = channel:getMessage(messageId)
+				if (message) then
+					local hasReaction = {}
+					for k,reaction in pairs(message.reactions) do
+						if (reaction.me) then
+							local emoji = bot:GetEmojiData(guild, reaction.emojiName)
+							if (emoji) then
+								hasReaction[emoji.Name] = true
+							else
+								self:LogError(guild, "Found reaction which does not exist in guild: %s", reaction.emojiName)
+							end
+						end
 					end
-				end
-			end
-			
-			for k,reactionInfo in pairs(reactionTable) do
-				local emoji = bot:GetEmojiData(guild, reactionInfo.reaction)
-				if (emoji) then
-					if (not hasReaction[emoji.Name] and not message:addReaction(emoji)) then
-						self:LogWarning(guild, "Failed to add reaction %s on message %s (channel: %s)", tostring(emojiName), message.id, message.channel.id)
+					
+					for reaction, _ in pairs(reactionTable) do
+						local emoji = bot:GetEmojiData(guild, reaction)
+						if (emoji) then
+							if (not hasReaction[emoji.Name] and not message:addReaction(emoji.Emoji or emoji.Id)) then
+								self:LogWarning(guild, "Failed to add reaction %s on message %s (channel: %s)", emoji.Name, message.id, message.channel.id)
+							end
+						else
+							self:LogError(guild, "Emoji \"%s\" does not exist", reactionInfo.reaction)
+						end
 					end
 				else
-					self:LogError(guild, "Emoji \"%s\" does not exist", reactionInfo.reaction)
+					self:LogError(guild, "Message %s no longer exists in channel %s", messageId, channelId)
 				end
 			end
+		else
+			self:LogError(guild, "Channel %s no longer exists", channelId)
 		end
 	end
 
@@ -270,7 +474,7 @@ function Module:HandleReactionRemove(guild, userId, channelId, messageId, reacti
 		if (role) then
 			self:LogInfo(guild, "Removing %s%s from %s", role.name, (role.color ~= 0) and " (colored)" or "", member.tag)
 			if (not member:removeRole(roleId)) then
-				self:LogWarning(guild, "Failed to remove role % from %s", role.name, member.tag)
+				self:LogWarning(guild, "Failed to remove role %s from %s", role.name, member.tag)
 			end
 		else
 			self:LogWarning(guild, "Role %s appears to have been removed", roleId)
@@ -282,7 +486,7 @@ function Module:HandleReactionRemove(guild, userId, channelId, messageId, reacti
 		if (role) then
 			self:LogInfo(guild, "Adding back %s%s to %s", role.name, (role.color ~= 0) and " (colored)" or "", member.tag)
 			if (not member:addRole(roleId)) then
-				self:LogWarning(guild, "Failed to add role % to %s", role.name, member.tag)
+				self:LogWarning(guild, "Failed to add role %s to %s", role.name, member.tag)
 			end
 		else
 			self:LogWarning(guild, "Role %s appears to have been removed", roleId)
