263ac21d214a10cef8a100b969d9374b8c573481
Lynix <lynix680@gmail.com>
2020-04-17 14:49:46 +0200

    Add modmail

---
+++ module_modmail.lua
@@ -0,0 +1,263 @@
+-- Copyright (C) 2020 Jérôme Leclercq
+-- This file is part of the "Not a Bot" application
+-- For conditions of distribution and use, see copyright notice in LICENSE
+
+local bot = Bot
+local client = Client
+local discordia = Discordia
+local enums = discordia.enums
+
+discordia.extensions() -- load all helpful extensions
+
+Module.Name = "modmail"
+
+function Module:GetConfigTable()
+	return {
+		{
+			Name = "Category",
+			Description = "Where should modmail channels be created",
+			Type = bot.ConfigType.Category,
+			Default = ""
+		},
+		{
+			Name = "ArchiveCategory",
+			Description = "Category where modmail channels are moved to when closed",
+			Type = bot.ConfigType.Category,
+			Optional = true
+		},
+		{
+			Name = "LogChannel",
+			Description = "Where should modmail logs should be stored",
+			Type = bot.ConfigType.Channel,
+			Optional = true
+		},
+		{
+			Name = "MaxConcurrentChannels",
+			Description = "How many concurrents (active) channels can be created",
+			Type = bot.ConfigType.Integer,
+			Default = 10
+		},
+		{
+			Name = "DeleteDuration",
+			Description = "How many time does a ticket channel take to be deleted after being closed",
+			Type = bot.ConfigType.Duration,
+			Default = 24 * 60 * 60
+		}
+	}
+end
+
+function Module:OnLoaded()
+	self.Clock = discordia.Clock()
+	self.Clock:on("sec", function ()
+		local now = os.time()
+		self:ForEachGuild(function (guildId, config, data, persistentData)
+			local guild = client:getGuild(guildId)
+			if (guild) then
+				local config = self:GetConfig(guild)
+				local deleteDuration = config.DeleteDuration
+
+				local data = self:GetPersistentData(guild)
+
+				local archiveData = data.archivedChannels[1]
+				if (archiveData and now >= archiveData.closedAt + deleteDuration) then
+					table.remove(data.archivedChannels, 1)
+
+					local channel = guild:getChannel(archiveData.channelId)
+
+					if (channel) then
+						channel:delete()
+					end
+				end
+			end
+		end)
+	end)
+
+	self:RegisterCommand({
+		Name = "newticket",
+		Args = {},
+
+		Help = "Allows you to contact the server staff in private",
+		Silent = true,
+		Func = function (commandMessage, configMessage)
+			local guild = commandMessage.guild
+			local config = self:GetConfig(guild)
+			local data = self:GetPersistentData(guild)
+
+			local user = commandMessage.author
+
+			if (data.activeChannels[user.id]) then
+				commandMessage:reply(string.format("You already have an active ticket on this server %s", user.mentionString))
+				return
+			end
+
+			if (config.MaxConcurrentChannels > 0 and table.count(data.activeChannels) > config.MaxConcurrentChannels) then
+				commandMessage:reply(string.format("Sorry %s, but there are actually too many tickets open at the same time, please retry in a moment", commandMessage.author.mentionString))
+				return
+			end
+
+			local modmailCategory = guild:getChannel(config.Category)
+			if (not modmailCategory or modmailCategory.type ~= enums.channelType.category) then
+				commandMessage:reply("This server is not well configured, please tell the admins!")
+				return
+			end
+
+			local newChannel = modmailCategory:createTextChannel(string.format("%s-%s", user.username:sub(1, 8), user.discriminator))
+			if (not newChannel) then
+				commandMessage:reply("Failed to create the channel, this is likely a bug.")
+				return
+			end
+
+			local permissions = newChannel:getPermissionOverwriteFor(commandMessage.member)
+			if (not permissions) then
+				commandMessage:reply("Failed to create the channel, this is likely a bug.")
+				return
+			end
+
+			if (not permissions:setPermissions(bit.bor(enums.permission.readMessages, enums.permission.sendMessages), 0)) then				commandMessage:reply("Failed to create the channel, this is likely a bug.")
+				commandMessage:reply("Failed to create the channel, this is likely a bug.")
+				return
+			end
+
+			local activeChannelData = {
+				createdAt = os.time(),
+				channelId = newChannel.id
+			}
+
+			data.activeChannels[user.id] = activeChannelData
+
+			local message = newChannel:send(string.format("Hello %s, use this private channel to communicate with **%s** staff.\n\nStaff can react on this message with 👋 to close the ticket", user.mentionString, guild.name))
+			message:addReaction("👋")
+			message:pin()
+
+			activeChannelData.topMessageId = message.id
+		end
+	})
+
+	return true
+end
+
+function Module:OnReady()
+	self.Clock:start()
+end
+
+function Module:OnUnload()
+	if (self.Clock) then
+		self.Clock:stop()
+	end
+end
+
+function Module:OnEnable(guild)
+	local config = self:GetConfig(guild)
+	local modmailCategory = guild:getChannel(config.Category)
+	if (not modmailCategory or modmailCategory.type ~= enums.channelType.category) then
+		return false, "Invalid modmail category (check your configuration)"
+	end
+
+	local data = self:GetPersistentData(guild)
+	data.activeChannels = data.activeChannels or {}
+	data.archivedChannels = data.archivedChannels or {}
+
+	return true
+end
+
+function Module:HandleEmojiAdd(userId, message, reactionName)
+	if (userId == client.user.id) then
+		-- Ignore self
+		return
+	end
+
+	if (reactionName ~= "👋") then
+		return
+	end
+
+	local user = client:getUser(userId)
+	if (not user) then
+		return
+	end
+
+	local guild = message.guild
+	local data = self:GetPersistentData(guild)
+
+	for userId, channelData in pairs(data.activeChannels) do
+		if (channelData.topMessageId == message.id) then
+			if (user.id == userId) then
+				return
+			end
+
+			local archiveData = channelData
+
+			local config = self:GetConfig(guild)
+
+			local channel = guild:getChannel(channelData.channelId)
+			channel:sendf("%s has closed the ticket, this channel will automatically be deleted in about %s", user.mentionString, util.FormatTime(config.DeleteDuration, 2))
+
+			channel:setName(channel.name .. "✅")
+
+			data.activeChannels[userId] = nil
+			table.insert(data.archivedChannels, {
+				channelId = channel.id,
+				closedAt = os.time()
+			})
+
+			if (config.ArchiveCategory) then
+				local archiveCategory = guild:getChannel(config.ArchiveCategory)
+				if (archiveCategory and archiveCategory.type == enums.channelType.category) then
+					channel:setCategory(config.ArchiveCategory)
+				end
+			end
+
+			local ticketMember = guild:getMember(userId)
+			if (ticketMember) then
+				local permissions = channel:getPermissionOverwriteFor(ticketMember)
+
+				if (not permissions or not permissions:setPermissions(enums.permission.readMessages, enums.permission.sendMessages)) then				commandMessage:reply("Failed to create the channel, this is likely a bug.")
+					channel:sendf("Failed to deny send messages permission to %s.", commandMessage.member)
+				end
+
+				if (not permissions or not permissions:denyPermissions(enums.permission.sendMessages)) then
+					channel:sendf("Failed to deny send messages permission to %s.", commandMessage.member)
+				end
+			end
+
+			return
+		end
+	end
+end
+
+function Module:OnReactionAdd(reaction, userId)
+	local message = reaction.message
+	if (message.channel.type ~= enums.channelType.text) then
+		return
+	end
+
+	self:HandleEmojiAdd(userId, message, reaction.emojiName)
+end
+
+function Module:OnReactionAddUncached(channel, messageId, reactionIdOrName, userId)
+	if (channel.type ~= enums.channelType.text) then
+		return
+	end
+
+	local message = channel:getMessage(messageId)
+	if (not message) then
+		return
+	end
+
+	self:HandleEmojiAdd(userId, message, reactionIdOrName)
+end
+
+function Module:OnChannelDelete(channel)
+	if (channel.type ~= enums.channelType.text) then
+		return
+	end
+
+	local guild = channel.guild
+
+	local data = self:GetPersistentData(guild)
+	for userId, channelData in pairs(data.activeChannels) do
+		if (channelData.channelId == channel.id) then
+			data.activeChannels[userId] = nil
+			break
+		end
+	end
+end
