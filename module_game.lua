78179de5543a001174727b65b0028f44a74b534c
Lynix <lynix680@gmail.com>
2020-05-08 19:09:21 +0200

    Game: Put game list as a global configuration

--- module_game.lua
+++ module_game.lua
@@ -2,12 +2,26 @@
 -- This file is part of the "Not a Bot" application
 -- For conditions of distribution and use, see copyright notice in LICENSE
 
+local bot = Bot
 local client = Client
-local config = Config
 local discordia = Discordia
 
 Module.Name = "game"
 Module.Global = true
+
+function Module:GetConfigTable()
+	return {
+		{
+			Array = true,
+			Global = true,
+			Name = "GameList",
+			Description = "List of games",
+			Type = bot.ConfigType.String,
+			Default = {},
+			Sensitive = true
+		},
+	}
+end
 
 function Module:OnLoaded()
 	self.Clock = discordia.Clock()
@@ -30,7 +44,7 @@ function Module:OnUnload()
 end
 
 function Module:UpdateGame()
-	local games = config.GameModuleConfig
+	local games = self.GlobalConfig.GameList
 	local newGame = games[math.random(1, #games)]
 	if (type(newGame) == "function") then
 		newGame = newGame()
