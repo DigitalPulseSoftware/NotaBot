374314a411b5b58a689398e9952359aac321509e
Jérôme Leclercq <lynix680@gmail.com>
2021-12-02 15:11:23 +0100

    PM: Add possibility of answering private messages

--- module_pm.lua
+++ module_pm.lua
@@ -14,13 +14,6 @@ function Module:GetConfigTable()
 	return {
 		{
 			Global = true,
-			Name = "TargetGuild",
-			Description = "Where private messages should be logged",
-			Type = bot.ConfigType.Guild,
-			Optional = true
-		},
-		{
-			Global = true,
 			Name = "TargetChannel",
 			Description = "Where private messages should be logged",
 			Type = bot.ConfigType.Channel,
@@ -29,9 +22,133 @@ function Module:GetConfigTable()
 	}
 end
 
+function Module:HandleResponse(message)
+	local function Failure(text, ...)
+		message:reply({
+			content = string.format("❌ " .. text, ...),
+			reference = {
+				message = message,
+				mention = true
+			}
+		})
+	end
+
+	if message.type ~= enums.messageType.reply then
+		return Failure("You must reply to a message so I know whom to send your message")
+	end
+
+	local mirrorMessage = message.referencedMessage
+	if not mirrorMessage then
+		return Failure("Failed to retrieve referenced message, maybe its too old?")
+	end
+
+	local mirrorMessageId = mirrorMessage.id
+
+	local persistentData = self:GetPersistentData()
+	local mirrorData = persistentData.mirrors[mirrorMessageId]
+	p(mirrorData)
+
+	if not mirrorData then
+		return Failure("Failed to identify message, maybe its too old?")
+	end
+
+	local user, err = client:getUser(mirrorData.userId)
+	if not user then
+		return Failure("Failed to get user: %s", err)
+	end
+
+	local privateMessageChannel, err = user:getPrivateChannel()
+	if not privateMessageChannel then
+		return Failure("Failed to get private channel: %s", err)
+	end
+
+	local authorData = persistentData.users[mirrorData.userId]
+	if not authorData then
+		return Failure("An internal error occurred (no author data found)")
+	end
+
+	local reply, err = privateMessageChannel:send({
+		attachments = message.attachments,
+		content = message.content,
+		reference = {
+			message = authorData.lastMessageId ~= mirrorData.messageId and mirrorData.messageId or nil -- only reply if we're answering another message than the last one
+		}
+	})
+
+	if not reply then
+		return Failure("Failed to send reply to user: %s", err)
+	end
+
+	local success, err = message:addReaction("✅")
+	if not success then
+		self:LogError("failed to add confirmation reaction to message: %s", err)
+	end
+end
+
+function Module:HandlePrivateMessage(message)
+	if not self.GlobalConfig.TargetChannel then
+		return
+	end
+
+	local logChannel = client:getChannel(self.GlobalConfig.TargetChannel)
+	if not logChannel then
+		self:LogError("invalid target channel (%s)", self.GlobalConfig.TargetGuild)
+		return
+	end
+
+	local authorId = message.author.id
+	local messageId = message.id
+
+	local embed = Bot:BuildQuoteEmbed(message)
+	embed.footer = {
+		text = string.format("Author ID: %s | Message ID: %s", authorId, messageId)
+	}
+
+	local mirrorMessage, err = logChannel:send({
+		embed = embed
+	})
+	if not mirrorMessage then
+		self:LogError("failed to log private message: %s", err)
+		return
+	end
+
+	local mirrorMessageId = mirrorMessage.id
+
+	local persistentData = self:GetPersistentData()
+	persistentData.mirrors[mirrorMessageId] = {
+		userId = authorId,
+		messageId = messageId
+	}
+
+	local authorData = persistentData.users[authorId]
+	if not authorData then
+		authorData = {
+			mirrorMessages = {}
+		}
+		persistentData.users[authorId] = authorData
+	end
+
+	authorData.lastMessageId = messageId
+	table.insert(authorData.mirrorMessages, mirrorMessageId)
+
+	-- Keep only 100 previous message IDs per user (just in case)
+	while #authorData.mirrorMessages > 100 do
+		local mirrorMessageId = table.remove(authorData.mirrorMessages, 1)
+		persistentData.mirrors[mirrorMessageId] = nil
+	end
+end
+
+function Module:OnLoaded()
+	local persistentData = self:GetPersistentData()
+	persistentData.mirrors = persistentData.mirrors or {}
+	persistentData.users = persistentData.users or {}
+
+	return true
+end
+
 function Module:OnMessageCreate(message)
 	local channel = message.channel
-	if not channel or channel.type ~= enums.channelType.private then
+	if not channel then
 		return
 	end
 
@@ -40,28 +157,9 @@ function Module:OnMessageCreate(message)
 		return
 	end
 
-	if not self.GlobalConfig.TargetGuild or not self.GlobalConfig.TargetChannel then
-		return
-	end
-
-	local logGuild = client:getGuild(self.GlobalConfig.TargetGuild)
-	if not logGuild then
-		self:LogError("invalid target guild (%s)", self.GlobalConfig.TargetGuild)
-		return
-	end
-
-	local logChannel = logGuild:getChannel(self.GlobalConfig.TargetChannel)
-	if not logChannel then
-		self:LogError("invalid target channel (%s)", self.GlobalConfig.TargetGuild)
-		return
-	end
-
-	local embed = Bot:BuildQuoteEmbed(message, { bigAvatar = true })
-
-	local success, err = logChannel:send({
-		embed = embed
-	})
-	if not success then
-		self:LogError("failed to log private message: %s", err)
+	if channel.type == enums.channelType.private then
+		self:HandlePrivateMessage(message)
+	elseif channel.id == self.GlobalConfig.TargetChannel then
+		self:HandleResponse(message)
 	end
 end
