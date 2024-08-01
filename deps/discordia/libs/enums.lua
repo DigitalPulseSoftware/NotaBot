7eca8d41a9bdc9018ac184c241ce6cb391b3f792
Mjöllnir <github+perso@mjollnir.fr>
2024-08-01 02:39:38 +0200

    update: discordia
    - fix unhandled events VOICE_CHANNEL_STATUS_UPDATE and VOICE_CHANNEL_STATUS_DELETE
    - update enums with most recent values
    - add some doc

--- deps/discordia/libs/enums.lua
+++ deps/discordia/libs/enums.lua
@@ -46,6 +46,7 @@ enums.defaultAvatar = enum {
 	green   = 2,
 	orange  = 3,
 	red     = 4,
+	pink    = 5,
 }
 
 enums.notificationSetting = enum {
@@ -100,6 +101,25 @@ enums.messageType = enum {
 	contextMenuCommand                      = 23,
 	autoModerationAction                    = 24,
 	roleSubscriptionPurchase                = 25,
+	interactionPremiumUpsell                = 26,
+	stageStart                              = 27,
+	stageEnd                                = 28,
+	stageSpeaker                            = 29,
+--  unused                                  = 30,
+	stageTopic                              = 31,
+	applicationPremiumSubscription          = 32,
+--  unused                                  = 33,
+--  unused                                  = 34,
+--  unused                                  = 35,
+	guildIncidentAlertModeEnabled           = 36,
+	guildIncidentAlertModeDisabled          = 37,
+	guildIncidentReportRaid                 = 38,
+	guildIncidentReportFalseAlarm           = 39,
+--  unused                                  = 40,
+--  unused                                  = 41,
+--  unused                                  = 42,
+--  unused                                  = 43,
+	purchaseNotification                    = 44,
 }
 
 enums.relationshipType = enum {
@@ -108,6 +128,7 @@ enums.relationshipType = enum {
 	blocked         = 2,
 	pendingIncoming = 3,
 	pendingOutgoing = 4,
+	implicit        = 5,
 }
 
 enums.activityType = enum {
@@ -145,6 +166,7 @@ enums.gameType = enum { -- NOTE: deprecated; use activityType
 	competing = 5,
 }
 
+-- https://discord.com/developers/docs/resources/guild#guild-object-verification-level
 enums.verificationLevel = enum {
 	none     = 0,
 	low      = 1,
@@ -153,12 +175,14 @@ enums.verificationLevel = enum {
 	veryHigh = 4, -- ┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻
 }
 
+-- https://discord.com/developers/docs/resources/guild#guild-object-explicit-content-filter-level
 enums.explicitContentLevel = enum {
 	none   = 0,
 	medium = 1,
 	high   = 2,
 }
 
+-- https://discord.com/developers/docs/resources/guild#guild-object-premium-tier
 enums.premiumTier = enum {
 	none  = 0,
 	tier1 = 1,
@@ -166,61 +190,80 @@ enums.premiumTier = enum {
 	tier3 = 3,
 }
 
+-- https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
 enums.permission = enum {
-	createInstantInvite = flag64(0),
-	kickMembers         = flag64(1),
-	banMembers          = flag64(2),
-	administrator       = flag64(3),
-	manageChannels      = flag64(4),
-	manageGuild         = flag64(5),
-	addReactions        = flag64(6),
-	viewAuditLog        = flag64(7),
-	prioritySpeaker     = flag64(8),
-	stream              = flag64(9),
-	viewChannel         = flag64(10),
-	sendMessages        = flag64(11),
-	sendTextToSpeech    = flag64(12),
-	manageMessages      = flag64(13),
-	embedLinks          = flag64(14),
-	attachFiles         = flag64(15),
-	readMessageHistory  = flag64(16),
-	mentionEveryone     = flag64(17),
-	useExternalEmojis   = flag64(18),
-	viewGuildInsights   = flag64(19),
-	connect             = flag64(20),
-	speak               = flag64(21),
-	muteMembers         = flag64(22),
-	deafenMembers       = flag64(23),
-	moveMembers         = flag64(24),
-	useVoiceActivity    = flag64(25),
-	changeNickname      = flag64(26),
-	manageNicknames     = flag64(27),
-	manageRoles         = flag64(28),
-	manageWebhooks      = flag64(29),
-	manageEmojis        = flag64(30),
-	useSlashCommands    = flag64(31),
-	requestToSpeak      = flag64(32),
-	manageEvents        = flag64(33),
-	manageThreads       = flag64(34),
-	usePublicThreads    = flag64(35),
-	usePrivateThreads   = flag64(36),
-	useExternalStickers = flag64(37),
-	sendMessagesInThreads = flag64(38),
-	startEmbeddedActivities = flag64(39),
-	moderateMembers         = flag64(40)
+	createInstantInvite              = flag64(0),
+	kickMembers                      = flag64(1),
+	banMembers                       = flag64(2),
+	administrator                    = flag64(3),
+	manageChannels                   = flag64(4),
+	manageGuild                      = flag64(5),
+	addReactions                     = flag64(6),
+	viewAuditLog                     = flag64(7),
+	prioritySpeaker                  = flag64(8),
+	stream                           = flag64(9),
+	viewChannel                      = flag64(10),
+	sendMessages                     = flag64(11),
+	sendTextToSpeech                 = flag64(12),
+	manageMessages                   = flag64(13),
+	embedLinks                       = flag64(14),
+	attachFiles                      = flag64(15),
+	readMessageHistory               = flag64(16),
+	mentionEveryone                  = flag64(17),
+	useExternalEmojis                = flag64(18),
+	viewGuildInsights                = flag64(19),
+	connect                          = flag64(20),
+	speak                            = flag64(21),
+	muteMembers                      = flag64(22),
+	deafenMembers                    = flag64(23),
+	moveMembers                      = flag64(24),
+	useVoiceActivity                 = flag64(25),
+	changeNickname                   = flag64(26),
+	manageNicknames                  = flag64(27),
+	manageRoles                      = flag64(28),
+	manageWebhooks                   = flag64(29),
+	manageEmojis                     = flag64(30),
+	useSlashCommands                 = flag64(31),
+	requestToSpeak                   = flag64(32),
+	manageEvents                     = flag64(33),
+	manageThreads                    = flag64(34),
+	usePublicThreads                 = flag64(35),
+	usePrivateThreads                = flag64(36),
+	useExternalStickers              = flag64(37),
+	sendMessagesInThreads            = flag64(38),
+	startEmbeddedActivities          = flag64(39),
+	moderateMembers                  = flag64(40),
+	viewCreatorMonetizationAnalitics = flag64(41),
+	useSoundBoard                    = flag64(42),
+	createGuildExpressions           = flag64(43),
+	createEvents                     = flag64(44),
+	useExternalSounds                = flag64(45),
+	sendVoiceMessages                = flag64(46),
+--  unused                           = flag64(47),
+	setVoiceChannelStatus            = flag64(48),
+	sendPolls                        = flag64(49),
+	useExternalApps                  = flag64(50),
 }
 
 enums.messageFlag = enum {
-	crossposted          = flag(0),
-	isCrosspost          = flag(1),
-	suppressEmbeds       = flag(2),
-	sourceMessageDeleted = flag(3),
-	urgent               = flag(4),
-	hasThread            = flag(5),
-	ephemeral            = flag(6),
-	loading              = flag(7),
+	crossposted                = flag(0),
+	isCrosspost                = flag(1),
+	suppressEmbeds             = flag(2),
+	sourceMessageDeleted       = flag(3),
+	urgent                     = flag(4),
+	hasThread                  = flag(5),
+	ephemeral                  = flag(6),
+	loading                    = flag(7),
+	threadFailedToMentionRoles = flag(8),
+--  unused                     = flag(9),
+--  unused                     = flag(10),
+--  unused                     = flag(11),
+	suppressNotification       = flag(12),
+	isVoiceMessage             = flag(13),
 }
 
+-- https://discord.com/developers/docs/topics/gateway#list-of-intents
+-- https://discord.com/developers/docs/topics/gateway#privileged-intents
 enums.gatewayIntent = enum {
 	guilds                = flag(0),
 	guildMembers          = flag(1), -- privileged
@@ -244,8 +287,11 @@ enums.gatewayIntent = enum {
 	-- unused             = flag(19),
 	autoModConfiguration  = flag(20),
 	autoModExecution      = flag(21),
+	guildMessagePolls     = flag(24),
+	directMessagePolls    = flag(25),
 }
 
+-- https://discord.com/developers/docs/resources/audit-log#audit-log-entry-object-audit-log-events
 enums.actionType = enum {
 	guildUpdate            = 1,
 	channelCreate          = 10,
@@ -300,6 +346,17 @@ enums.actionType = enum {
 	autoModMessageBlock    = 143,
 	autoModMessageFlag     = 144,
 	autoModUserTimeout     = 145,
+	creatorMonetizationRequestCreated = 150,
+	creatorMonetizationTermsAccepted  = 151,
+	onboardingPromptCreated  = 163,
+	onboardingPromptUpdated  = 164,
+	onboardingPromptDeleted  = 165,
+	onboardingCreate         = 166,
+	onboardingUpdate         = 167,
+	homeSettingsCreate       = 190,
+	homeSettingsUpdate       = 191,
+	voiceChannelStatusUpdate = 192,
+	voiceChannelStatusDelete = 193,
 }
 
 enums.logLevel = enum {
@@ -310,33 +367,45 @@ enums.logLevel = enum {
 	debug   = 4,
 }
 
---- Backported from dev
+-- https://discord.com/developers/docs/resources/user#user-object-user-flags
 enums.userFlag = enum {
 	discordEmployee      = flag(0),
 	discordPartner       = flag(1),
 	hypesquadEvents      = flag(2),
 	bugHunterLevel1      = flag(3),
-	-- unused            = flag(4),
-	-- unused            = flag(5),
+--  unused               = flag(4),
+--  unused               = flag(5),
 	houseBravery         = flag(6),
 	houseBrilliance      = flag(7),
 	houseBalance         = flag(8),
 	earlySupporter       = flag(9),
 	teamUser             = flag(10),
-	-- unused            = flag(11),
+--  unused               = flag(11),
 	system               = flag(12),
-	-- unused            = flag(13),
+--  unused               = flag(13),
 	bugHunterLevel2      = flag(14),
-	-- unused            = flag(15),
+--  unused               = flag(15),
 	verifiedBot          = flag(16),
 	verifiedBotDeveloper = flag(17),
 	certifiedModerator   = flag(18),
+	botHttpInteractions  = flag(19),
+--  unused               = flag(20),
+--  unused               = flag(21),
+	activeDeveloper      = flag(22),
 }
 
+-- https://discord.com/developers/docs/resources/user#connection-object-visibility-types
+enums.visibilityType = enum {
+	none     = 0,
+	everyone = 1,
+}
+
+-- https://discord.com/developers/docs/resources/user#user-object-premium-types
 enums.premiumType = enum {
-	none = 0,
+	none         = 0,
 	nitroClassic = 1,
-	nitro = 2
+	nitro        = 2,
+	nitroBasic   = 3,
 }
 
 enums.commandOptionType = enum {
@@ -375,37 +444,53 @@ enums.interactionResponseType = enum {
 	modal                                = 9
 }
 
+-- https://discord.com/developers/docs/interactions/message-components#component-object-component-types
 enums.componentType = enum {
-	actionRow  = 1,
-	button     = 2,
-	selectMenu = 3,
-	textInput  = 4
+	actionRow         = 1,
+	button            = 2,
+	selectMenu        = 3, -- stringSelect
+	textInput         = 4,
+	userSelect        = 5,
+	roleSelect        = 6,
+	mentionableSelect = 7,
+	channelSelect     = 8,
 }
 
+-- https://discord.com/developers/docs/interactions/message-components#button-object-button-styles
 enums.buttonStyle = enum {
 	primary   = 1, -- blurple
 	secondary = 2, -- green
 	success   = 3, -- grey
 	danger    = 4, -- red
 	link      = 5, -- grey with link icon
+	premium   = 6, -- blurple + shop icon + SKU name + SKU price
 }
 
+-- https://discord.com/developers/docs/interactions/message-components#text-input-object-text-input-styles
 enums.textInputStyle = enum {
-	short = 1,
-	paragraph = 2
+	short     = 1,
+	paragraph = 2,
 }
 
+-- https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-response-object-messages
 enums.interactionResponseFlag = enum {
-	ephemeral = flag(6),
+	suppressEmbeds        = flag(2),
+	ephemeral             = flag(6),
+	suppressNotifications = flag(12),
 }
 
+-- https://discord.com/developers/docs/resources/application#application-object-application-flags
 enums.applicationFlag = enum {
-	gatewayPresence               = flag(12),
-	gatewayPresenceLimited        = flag(13),
-	gatewayGuildMembers           = flag(14),
-	gatewayGuildMembersLimited    = flag(15),
-	verificationPendingGuildLimit = flag(16),
-	embedded                      = flag(17),
+	applicationAutoModRuleCreateBadge = flag(6),
+	gatewayPresence                   = flag(12),
+	gatewayPresenceLimited            = flag(13),
+	gatewayGuildMembers               = flag(14),
+	gatewayGuildMembersLimited        = flag(15),
+	verificationPendingGuildLimit     = flag(16),
+	embedded                          = flag(17),
+	gatewayMessageContent             = flag(18),
+	gatewayMessageContentLimited      = flag(19),
+	applicationCommandBadge           = flag(23),
 }
 
 -- https://discord.com/developers/docs/resources/guild-scheduled-event#guild-scheduled-event-object-guild-scheduled-event-privacy-level
@@ -416,8 +501,8 @@ enums.scheduledEventsPrivacyLevel = enum {
 -- https://discord.com/developers/docs/resources/guild-scheduled-event#guild-scheduled-event-object-guild-scheduled-event-entity-types
 enums.scheduledEventsEntityTypes = enum {
 	stage_instance = 1,
-	voice = 2,
-	external = 3,
+	voice          = 2,
+	external       = 3,
 }
 
 return enums
